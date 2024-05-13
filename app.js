badd332ce06488a1dbd0d599331a9dce2298de8b
Douglas <155904691+kuandry@users.noreply.github.com>
2024-05-13 18:29:26 -0300

    aula002

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-alert("Bem vindo ao jogo do Número secreto");
-let numeroSecreto = 4;
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+alert("Olá! Espero que esteja bem!");
 
-// enquanto chute não for igual ao número secreto...
-while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 1 e 10");
+let nome = prompt("Qual seu nome");
+console.log(nome);
+let dia = prompt("Que dia é hoje?");
+let sexta = dia === 'sexta';
 
-  // Convertendo a entrada do usuário para um número
-  chute = Number(chute);
-
-  // se chute for igual ao número secreto
-  if (chute === numeroSecreto) {
-    alert(`Parabéns! Você acertou em cheio ${numeroSecreto}!com ${tentativas} tentativas`);
-  } else {
-    if (chute > numeroSecreto) {
-      alert(`O número secreto é menor do que ${chute}`);
-    } else {
-      alert(`O número secreto é maior do que ${chute}`);
-    }
-/*     tentativas = tentativas +1;
- */    tentativas ++
+if (
+  (dia === "sabado" || dia === "domingo")
+) {
+  console.log(nome + " bom final de semana!");
+} else {
+  if (sexta) console.log(nome + " sextou! Bora tomar uma!");
+  else {
+    nome + " Boa semana";
   }
 }
